36abbeb4a373b5f9227ee84e51640e24b8b7f185
HVQAGit <mohan.goud@hitachivantara.com>
2026-07-22 09:30:21 +0530

    9:30

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -46,8 +46,10 @@ constructor(page:Page){  // //constructor is public because to allow other/exter
 
 async goToLoginPage():Promise<void> {
  
-   
+    console.log("BASE_URL==");
+  console.log(process.env.BASE_URL!);
     await this.page.goto(process.env.BASE_URL!);
+
    
 }
 
