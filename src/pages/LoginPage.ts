38407ace5c04cd0d04c4836b0172623f817da7ae
HVQAGit <mohan.goud@hitachivantara.com>
2026-07-22 11:31:39 +0530

    11:31

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -50,9 +50,11 @@ async goToLoginPage():Promise<void> {
 
   console.log(process.env.BASE_URL!);
    console.log("BUSERNAME==");
-   console.log(process.env.USERNAME!);
+   console.log(process.env.USERNAME!.length);
    console.log("PASSWORD==");
-   console.log(process.env.PASSWORD!);
+   console.log(process.env.PASSWORD!.length);
+   
+  
   await this.page.goto(process.env.BASE_URL!);
 
    
